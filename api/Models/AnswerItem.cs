4e64704116aca50060e057920f405656ec25acd6
Eduardo <eduardotech@outlook.com>
2023-01-28 22:32:28 +1000

    answerId is not null

--- api/Models/AnswerItem.cs
+++ api/Models/AnswerItem.cs
@@ -3,7 +3,7 @@
     public partial class AnswerItem
     {
         public int Id { get; set; }
-        public int? AnswerId { get; set; }
+        public int AnswerId { get; set; }
         public string Title { get; set; }
         public string Subtitle { get; set; }
         public string Question { get; set; }
