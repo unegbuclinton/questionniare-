4c452f54a979d6f9c699be3af0395e420f5d29e3
Eduardo <eduardotech@outlook.com>
2023-01-28 22:22:41 +1000

    added cors

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
   const value = 'World';
-  axios.get('api/GetQuestionnaire').then((res)=>{
+  axios.get('https://calm-smoke-0cbb30f00.2.azurestaticapps.net/api/GetQuestionnaire').then((res)=>{
     console.log(res.data,"my data");
   });
   return <div>Hello {value}</div>;
